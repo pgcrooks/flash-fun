46ffbb5374b7849bd4caec3ec080103502972ae4
Paul Crooks <pgcrooks@gmail.com>
2023-03-21 21:34:24 +0000

    Add user APIs

--- hello.py
+++ hello.py
@@ -3,6 +3,21 @@ from markupsafe import escape
 
 app = Flask(__name__)
 
+class User:
+    def __init__(self, username, group):
+        self.username = username
+        self.group = group
+
+def get_current_user():
+    user = User("Test User", "Test Group")
+    return user
+
+def get_all_users():
+    return [
+        User("Test User 1", "Test Group1 "),
+        User("Test User 2", "Test Group 2")
+    ]
+
 @app.route("/")
 def index():
     return "<p>Index Page</p>"
@@ -19,6 +34,19 @@ def show_post(post_id):
 def show_subpath(subpath):
     # show the subpath after /path/
     return f'Subpath {escape(subpath)}'
+
+@app.route("/me")
+def me():
+    user = get_current_user()
+    return {
+        "username": user.username,
+        "group": user.group,
+    }
+
+@app.route("/all_users")
+def users():
+    users = get_all_users()
+    return [user.username for user in users]
 
 @app.errorhandler(404)
 def page_not_found(error):
